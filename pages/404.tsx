4d6531e1bc4e345edc91ac3bb6a8158b311c8fe2
Smit Barmase <smitbarmase@outlook.com>
2020-11-30 09:31:11 +0530

    Update

--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-  return <>Not Found</>;
-};
-
-export default NotFound;
--- utils/ToText.ts
+++
@@ -1,6 +0,0 @@
-export default function ToText(node) {
-  let tag = document.createElement("div");
-  tag.innerHTML = node;
-  node = tag.innerText;
-  return node;
-}
