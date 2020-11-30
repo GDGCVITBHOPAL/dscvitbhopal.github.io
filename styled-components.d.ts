d7cfd890561cdf9174bebcd4306f4701399b7f4d
Smit Barmase <smitbarmase@outlook.com>
2020-11-30 09:30:26 +0530

    Remove

--- styled-components.d.ts
+++
@@ -1,7 +0,0 @@
-import { lightTheme } from "./styles/theme";
-
-type ThemeInterface = typeof lightTheme;
-
-declare module "styled-components" {
-  export interface DefaultTheme extends ThemeInterface {}
-}
