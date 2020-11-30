9ca2876885aac14252ae09b86739de9ae74d1e16
Smit Barmase <smitbarmase@outlook.com>
2020-11-30 09:15:24 +0530

    Prod config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
+const debug = process.env.NODE_ENV !== "production";
+
 module.exports = {
-  basePath: "",
-  assetPrefix: "//",
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      "/team": { page: "/team" },
+      "/articles": { page: "/articles" },
+      "/badges": { page: "/badges" },
+      "/events": { page: "/events" },
+    };
+  },
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    return config;
+  },
 };
