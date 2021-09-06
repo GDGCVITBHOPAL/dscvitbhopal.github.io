d0aac203d70e908cd35bae89c55098e5a5443a6e
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-09-07 01:15:56 +0530

    Updates

---
+++ _next/static/TjR7poEMfPUoAunsHkBxK/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fbadges\u002F[id]","\u002Fcertificates\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/events":["static\u002Fchunks\u002Fpages\u002Fevents.js"],"/team":["static\u002Fchunks\u002Fpages\u002Fteam.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fevents","\u002Fteam"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
